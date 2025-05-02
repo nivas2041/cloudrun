f440b6d2156283f1e9181b5bd57576dcf3f40258
AKSHAT GUPTA <akshu20791@gmail.com>
2025-05-02 14:41:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Welcome to Technical Guftgu Youtube Channel</h1>");
+  response.end("<h1> Welcome to THE CAREEERBEER WEBSITE</h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
