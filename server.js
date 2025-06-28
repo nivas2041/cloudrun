e682bbe41908d42aa900e9b0729ae65122740cf6
nivas2041 <96873189+nivas2041@users.noreply.github.com>
2025-06-28 10:55:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Welcome to THE CAREEERBEER WEBSITE</h1>");
+  response.end("<h1> Welcome to Training Website for Learning DevOps</h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
